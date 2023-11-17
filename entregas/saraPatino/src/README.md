f05b68063ce2b463c779941ff59ab2eae72ef226
Sara Aisha Pati�o Sol�s <sara.patino@unealum.local>
2023-11-17 13:52:07 +0100

    Se agrego la clase Lydia

---
+++ entregas/saraPatino/src/README.md
@@ -0,0 +1,1 @@
+Codigo fuente: LudotecaSimulacion.java
---
+++ entregas/saraPatino/src/parcial/Niño.java
@@ -0,0 +1,13 @@
+package parcial;
+
+class Niño {
+    private String nombre;
+
+    public Niño(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
