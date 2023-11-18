568c801a7d48bdebb022f17c0974c699c59704b0
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2023-11-18 19:42:31 +0100

    Update Aisha.java

--- entregas/martinezMaura/Ludoteca/src/Aisha.java
+++ entregas/martinezMaura/Ludoteca/src/Aisha.java
@@ -4,23 +4,27 @@ import java.util.List;
 
 class Aisha {
     public void jugar(List<Nino> cola) {
-       
-        System.out.println("Limpiando la pizarra...");
+        System.out.println("Limpiando la pizarra del salón...");
 
-        for (Nino nino : cola) {
-           
-            System.out.println("Mostrando mensaje a un niño: " + nino.getMensaje());
-            
+        for (int i = 0; i < cola.size(); i++) {
+            Nino nino = cola.get(i);
+
+            if (i == 0) {
+                System.out.println("Escribiendo un mensaje en el pizarrín y mostrándolo al primer niño: " + nino.getMensaje());
+            } else {
+                Nino ninoAnterior = cola.get(i - 1);
+                nino.recibirMensaje(ninoAnterior.getMensaje());
+                System.out.println("El niño " + ninoAnterior.getNombre() + " le muestra el mensaje al niño " + nino.getNombre());
+            }
+
             try {
-              Thread.sleep(1000); 
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("El último niño corre a escribir en la pizarra del salón...");
+
+        Nino ultimoNino = cola.get(cola.size() - 1);
+        System.out.println("El último niño, " + ultimoNino.getNombre() + ", corre a escribir en la pizarra del salón el mensaje que ha recibido: " + ultimoNino.getMensaje());
     }
 }
-            
-
-        
-
