b3709bf80ad76ad82682a0ee9d95e7ff5d0d198e
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2023-11-20 20:10:09 +0100

    feature: new functionality, added - the start of playroom - game handler - Console  handler - Waiting children Management

--- entregas/PlayRoom.java
+++ entregas/PlayRoom.java
@@ -1,17 +1,19 @@
+import java.util.Scanner;
+
 public class PlayRoom {
 
     private int hoursOpen = 2;
     private int minutesOpen = 60 * hoursOpen;
     private Monitor Lydia;
     private Monitor Aisha;
-    private ChalkBoard chalkBoard;
+    private ChalkBoard playRoomChalkBoard;
     private int actualMinute = 1;
     private int minuteCharge = 0;
 
     public PlayRoom() {
         this.Lydia = new Monitor();
         this.Aisha = new Monitor();
-        this.chalkBoard = new ChalkBoard();
+        this.playRoomChalkBoard = new ChalkBoard();
     }
 
     private int arrivedChilds() {
@@ -32,4 +34,65 @@ public class PlayRoom {
         return 0;
     }
 
+    private void manageWaitingChildren(int numberOfChilds, Monitor monitor) {
+        if (numberOfChilds > 0) {
+            int iterator = 1;
+            while (iterator <= numberOfChilds) {
+                monitor.setNewChild(new Child());
+                iterator++;
+            }
+        }
+    }
+
+    public void consoleHandler(int arrivedNumber){
+        System.out.print("|Num Of childs Arrived: " + arrivedNumber);
+        System.out.println(" | Actual Minute: " + this.actualMinute);
+        System.out.print("|Childs waiting: "+  Lydia.childList.size());
+        System.out.println(" | Childs playing: " + Aisha.childList.size());
+        
+        System.out.println(">>Press Enter for Next Minute<<");
+        Scanner inputScanner = new Scanner(System.in);
+        inputScanner.nextLine();
+    }
+
+    private void gameHandler(Monitor monitorPlayer, Monitor monitorChildKeeper){
+        int childWaiting = monitorChildKeeper.childList.size();
+
+        if (childWaiting > 5) {
+            if (!monitorPlayer.isPlaying()) {
+                monitorPlayer.setPlaying(true);
+                while (!monitorChildKeeper.isChildListEmpty()) {
+                    Child child = monitorChildKeeper.passFirstChild();
+    
+                    child.setChalkBoard(new ChalkBoard());
+    
+                    monitorPlayer.setNewChild(child);
+                }
+            }
+
+            if (monitorPlayer.isPlaying()) {
+                if (monitorPlayer.actualChildTurnNode == null) {
+                    monitorPlayer.cleanChalkBoard(this.playRoomChalkBoard);
+                    monitorPlayer.chalkBoard.setMessage("Hello, World!");
+                    monitorPlayer.monitorGame(playRoomChalkBoard);
+                }
+            }
+
+
+        }
+
+    }
+
+    public void openPlayRoom() {
+
+        while (this.actualMinute <= this.minutesOpen) {
+            int arrivedNumber = arrivedChilds();
+            this.manageWaitingChildren(arrivedNumber, Lydia);
+
+            this.gameHandler(Aisha,Lydia);
+
+            this.actualMinute = this.actualMinute + 1;
+            this.consoleHandler(arrivedNumber);
+        }
+    }
 }
