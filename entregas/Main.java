02eb2b539d48fbea9f900baa81ae2c1151e8a4a7
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2023-11-20 20:08:03 +0100

    fix: refactor main

--- entregas/Main.java
+++ entregas/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        PlayRoom playRoom = new PlayRoom();
-        
+        PlayRoom playRoomObj = new PlayRoom();
+        playRoomObj.openPlayRoom();
+        System.exit(0);
     }
 }
