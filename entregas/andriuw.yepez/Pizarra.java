7d8b84a382acbea560b1beb21deeee6f756ba7f0
andriuw08 <114088104+andriuw08@users.noreply.github.com>
2023-12-01 15:25:24 +0100

    Fix Examen Parcial

---
+++ entregas/andriuw.yepez/Pizarra.java
@@ -0,0 +1,11 @@
+public class Pizarra {
+    String nuevoMensaje;
+
+    public String getNuevoMensaje() {
+        return nuevoMensaje;
+    }
+
+    public void setNuevoMensaje(String nuevoMensaje) {
+        this.nuevoMensaje = nuevoMensaje;
+    }
+}
