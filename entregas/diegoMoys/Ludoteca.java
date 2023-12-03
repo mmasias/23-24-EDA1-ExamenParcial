a707c46af99d7230582cd157534d4c1a413e10b6
Diego <diegomoys01@gmail.com>
2023-12-03 17:19:08 +0100

    Ludoteca functionality

---
+++ entregas/diegoMoys/Ludoteca.java
@@ -0,0 +1,33 @@
+class Ludoteca {
+    private Monitor lydia;
+    private Monitor aisha;
+    private Pizarra pizarraDelSalon;
+
+    public Ludoteca(){
+        lydia = new Monitor("Lydia");
+        aisha = new Monitor("Aisha");
+        pizarraDelSalon = new Pizarra();
+    }
+
+    public void recibirNiño(Niño niño) {
+        lydia.recibeNiño(niño);
+    }
+
+    public void actualizar() {
+        gestionarColas();
+        if (aisha.puedeJugar()) {
+            aisha.jugar();
+        } 
+    }
+
+    private void gestionarColas() {
+        if (lydia.tieneNiños() && !aisha.estaJugando()) {
+            lydia.entregaNiños(aisha);
+        }        
+    }
+
+    public void verEstado(){
+        lydia.mostrarListaNiños();
+        aisha.mostrarListaNiños();
+    }
+}
